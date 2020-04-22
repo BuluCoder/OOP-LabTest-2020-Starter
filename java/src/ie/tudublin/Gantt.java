54cc99cae85eb5b90a3313d5d65218f7d74e5880
KBulu <55754565+BuluCoder@users.noreply.github.com>
2020-04-22 14:48:47 +0100

    update task.java and ensure loadtasks and printtasks work

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,9 +1,15 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+
 
 public class Gantt extends PApplet
-{	
+{
+	ArrayList<Task> tk = new ArrayList<Task>();
 	
 	public void settings()
 	{
@@ -12,12 +18,20 @@ public class Gantt extends PApplet
 
 	public void loadTasks()
 	{
-		
+		Table tab = loadTable("tasks.csv", "header");
+		for(TableRow tabrow:tab.rows())
+		{
+			Task task = new Task(tabrow);
+			tk.add(task);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+		for(Task tk:tk)
+		{
+				System.out.println(tk);
+		}
 	}
 	
 	public void mousePressed()
@@ -34,6 +48,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -1,4 +1,55 @@
 package ie.tudublin;
 
+import processing.data.TableRow;
+
 public class Task {
+    private String tk;
+    private  int srt;
+    private int end;
+
+    public String getTask()
+    {
+        return tk;
+    }
+
+    public void setTask(String task)
+    {
+        tk = task;
+    }
+
+    public  int getStart()
+    {
+        return srt;
+    }
+
+    public void setStart(int start)
+    {
+        srt = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public Task(String task, int start, int end)
+    {
+        tk = task;
+        srt = start;
+        this.end = end;
+    }
+
+    public Task(TableRow tabrow)
+    {
+        this(tabrow.getString("Task"), tabrow.getInt("Start"), tabrow.getInt("End"));
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Task = " + tk + ", Start = " + srt + ", End = " + end + ".";
+    }
 }
